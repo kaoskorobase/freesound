3311ba2f4063f4f2a3d1d8df3117d5df949b98bb
Stefan Kersten <sk@xdv.org>
2012-10-15 20:01:15 +0200

    Simplify HTTP request function

--- Sound/Freesound/Conduit.hs
+++ Sound/Freesound/Conduit.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
-module Sound.Freesound.Conduit where
+-- | A blah blah blah ...
+--
+-- > httpRequest :: (Failure HTTP.HttpException m, R.MonadBaseControl IO m, C.MonadResource m) =>
+-- >     HTTPRequest (R.ReaderT HTTP.Manager m)
+-- > httpRequest _ u _ = do
+-- >     liftIO $ putStrLn $ "Request: " ++ show u
+-- >     man <- R.ask
+-- >     req <- HTTP.parseUrl (show u)
+-- >     res <- HTTP.http req man
+-- >     return $ HTTP.responseBody res
+
+module Sound.Freesound.Conduit (
+) where
 
 import           Control.Failure (Failure)
-import           Control.Monad.IO.Class (liftIO)
-import qualified Control.Monad.Trans.Class as R
-import qualified Control.Monad.Trans.Control as R
-import qualified Control.Monad.Trans.Reader as R
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Control.Monad.Trans.Control as C
 import qualified Data.Aeson as J
 import qualified Data.ByteString as B
 import qualified Data.Conduit as C
@@ -20,25 +30,28 @@ import           Sound.Freesound.Sound
 import qualified Sound.Freesound.Sound.Search.Filter as F
 import           Sound.Freesound.URI
 
+apiKey :: APIKey
 apiKey = T.pack "9a585b9eb66b4e84b405f50e4a8185a1"
 
-printit :: Maybe Sounds -> IO ()
-printit x = putStrLn $ "Response: " ++ show x
+httpRequest :: (Failure HTTP.HttpException m, C.MonadBaseControl IO m, C.MonadResource m) =>
+    HTTP.Manager -> HTTPRequest m
+httpRequest man _ uri _ = do
+    liftIO $ putStrLn $ "httpRequest: " ++ show uri
+    HTTP.parseUrl (show uri)
+        >>= flip HTTP.http man
+        >>= return . HTTP.responseBody
 
-httpRequest :: (Failure HTTP.HttpException m, R.MonadBaseControl IO m, C.MonadResource m) =>
-    HTTPRequest (R.ReaderT HTTP.Manager m)
-httpRequest _ u _ = do
-    liftIO $ putStrLn $ "Request: " ++ show u
-    man <- R.ask
-    req <- HTTP.parseUrl (show u)
-    res <- HTTP.http req man
-    return $ HTTP.responseBody res
-
-withManager a = HTTP.withManager $ \man -> R.runReaderT a man
-withFreesound' = withManager . withFreesound httpRequest apiKey
+runFreesound ::
+    ( MonadIO m
+    , C.MonadUnsafeIO m
+    , C.MonadThrow m
+    , C.MonadBaseControl IO m
+    , Failure HTTP.HttpException m ) =>
+    FreesoundT (C.ResourceT m) a -> m a
+runFreesound a = HTTP.withManager $ \man -> withFreesound (httpRequest man) apiKey a
 
 main :: IO ()
-main = withFreesound' $ do
+main = runFreesound $ do
     let q = include "drum" & include "bass" & exclude "loop"
         f = mempty -- F.username "Dolfeus"
         s = Nothing
