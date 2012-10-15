ccaf0aed9cc3d9b87741609e1fee903586b25cf6
Stefan Kersten <sk@xdv.org>
2012-10-15 20:00:56 +0200

    Haddock markup fixes

--- Sound/Freesound/Sound.hs
+++ Sound/Freesound/Sound.hs
@@ -57,27 +57,27 @@ instance FromJSON FileType where
 type Tag = Text
 
 data SoundSummary = SoundSummary {
-    id                  :: SoundId                -- | The sound’s unique identifier.
-  , ref                 :: Resource Sound         -- | The URI for this sound.
-  , url                 :: Data                   -- | The URI for this sound on the Freesound website.
-  , preview_hq_mp3      :: Data                   -- | The URI for retrieving a high quality (~128kbps) mp3 preview of the sound.
-  , preview_lq_mp3      :: Data                   -- | The URI for retrieving a low quality (~64kbps) mp3 preview of the sound.
-  , preview_hq_ogg      :: Data                   -- | The URI for retrieving a high quality (~192kbps) ogg preview of the sound.
-  , preview_lq_ogg      :: Data                   -- | The URI for retrieving a low quality (~80kbps) ogg of the sound.
-  , serve               :: Data                   -- | The URI for retrieving the original sound.
-  , similarity          :: Resource Sounds        -- | URI pointing to the similarity resource (to get a list of similar sounds).
-  , fileType            :: FileType               -- | The type of sound (wav, aif, mp3, etc.).
-  , duration            :: Double                 -- | The duration of the sound in seconds.
-  , originalFilename    :: Text                   -- | The name of the sound file when it was uploaded.
-  , tags                :: [Tag]                  -- | An array of tags the user gave the sound.
-  , pack                :: Maybe (Resource Pack)  -- | If the sound is part of a pack, this URI points to that pack’s API resource.
-  , user                :: UserSummary            -- | A dictionary with the username, url, and ref for the user that uploaded the sound.
-  , spectral_m          :: Data                   -- | A visualization of the sounds spectrum over time, jpeg file (medium).
-  , spectral_l          :: Data                   -- | A visualization of the sounds spectrum over time, jpeg file (large).
-  , waveform_m          :: Data                   -- | A visualization of the sounds waveform, png file (medium).
-  , waveform_l          :: Data                   -- | A visualization of the sounds waveform, png file (large).
-  , analysisStats       :: Data                   -- | URI pointing to the analysis results of the sound (see Analysis Descriptor Documentation).
-  , analysisFrames      :: Data                   -- | The URI for retrieving a JSON file with analysis information for each frame of the sound (see Analysis Descriptor Documentation).
+    id                  :: SoundId                -- ^ The sound’s unique identifier.
+  , ref                 :: Resource Sound         -- ^ The URI for this sound.
+  , url                 :: Data                   -- ^ The URI for this sound on the Freesound website.
+  , preview_hq_mp3      :: Data                   -- ^ The URI for retrieving a high quality (~128kbps) mp3 preview of the sound.
+  , preview_lq_mp3      :: Data                   -- ^ The URI for retrieving a low quality (~64kbps) mp3 preview of the sound.
+  , preview_hq_ogg      :: Data                   -- ^ The URI for retrieving a high quality (~192kbps) ogg preview of the sound.
+  , preview_lq_ogg      :: Data                   -- ^ The URI for retrieving a low quality (~80kbps) ogg of the sound.
+  , serve               :: Data                   -- ^ The URI for retrieving the original sound.
+  , similarity          :: Resource Sounds        -- ^ URI pointing to the similarity resource (to get a list of similar sounds).
+  , fileType            :: FileType               -- ^ The type of sound (wav, aif, mp3, etc.).
+  , duration            :: Double                 -- ^ The duration of the sound in seconds.
+  , originalFilename    :: Text                   -- ^ The name of the sound file when it was uploaded.
+  , tags                :: [Tag]                  -- ^ An array of tags the user gave the sound.
+  , pack                :: Maybe (Resource Pack)  -- ^ If the sound is part of a pack, this URI points to that pack’s API resource.
+  , user                :: UserSummary            -- ^ A dictionary with the username, url, and ref for the user that uploaded the sound.
+  , spectral_m          :: Data                   -- ^ A visualization of the sounds spectrum over time, jpeg file (medium).
+  , spectral_l          :: Data                   -- ^ A visualization of the sounds spectrum over time, jpeg file (large).
+  , waveform_m          :: Data                   -- ^ A visualization of the sounds waveform, png file (medium).
+  , waveform_l          :: Data                   -- ^ A visualization of the sounds waveform, png file (large).
+  , analysisStats       :: Data                   -- ^ URI pointing to the analysis results of the sound (see Analysis Descriptor Documentation).
+  , analysisFrames      :: Data                   -- ^ The URI for retrieving a JSON file with analysis information for each frame of the sound (see Analysis Descriptor Documentation).
   } deriving (Eq, Show)
 
 instance FromJSON SoundSummary where
@@ -125,21 +125,21 @@ instance FromJSON Geotag where
 data Sound = Sound {
   sound_summary       :: SoundSummary
 
-, samplerate          :: Int          -- | The samplerate of the sound.
-, bitdepth            :: Int          -- | The bit depth of the sound.
-, filesize            :: Integer      -- | The size of the file in bytes.
-, bitrate             :: Int          -- | The bit rate of the sound.
-, channels            :: Int          -- | The number of cha-- nnel-- s-- .
+, samplerate          :: Int          -- ^ The samplerate of the sound.
+, bitdepth            :: Int          -- ^ The bit depth of the sound.
+, filesize            :: Integer      -- ^ The size of the file in bytes.
+, bitrate             :: Int          -- ^ The bit rate of the sound.
+, channels            :: Int          -- ^ The number of channels.
 
-, description         :: Text         -- | The description the user gave the sound.
-, license             :: Data         -- | The license under which the sound is available to you.
-, created             :: Text         -- | The date of when the sound was uploaded.
-, numComments         :: Int          -- | The number of comments.
-, numDownloads        :: Int          -- | The number of times the sound was downloaded.
-, numRatings          :: Int          -- | The number of times the sound was rated.
-, avgRating           :: Double       -- | The average rating of the sound.
+, description         :: Text         -- ^ The description the user gave the sound.
+, license             :: Data         -- ^ The license under which the sound is available to you.
+, created             :: Text         -- ^ The date of when the sound was uploaded.
+, numComments         :: Int          -- ^ The number of comments.
+, numDownloads        :: Int          -- ^ The number of times the sound was downloaded.
+, numRatings          :: Int          -- ^ The number of times the sound was rated.
+, avgRating           :: Double       -- ^ The average rating of the sound.
 
-, geotag              :: Maybe Geotag -- | A dictionary with the latitude (‘lat’) and longitude (‘lon’) of the geotag (only for sounds that have been geotagged).
+, geotag              :: Maybe Geotag -- ^ A dictionary with the latitude (‘lat’) and longitude (‘lon’) of the geotag (only for sounds that have been geotagged).
 } deriving (Eq, Show)
 
 instance FromJSON Sound where
@@ -180,9 +180,9 @@ instance FromJSON Sounds where
 
 -- | User of the Freesound database.
 data UserSummary = UserSummary {
-  username :: Text            -- | The user’s username.
-, user_ref  :: Resource User  -- | The URI for this resource.
-, user_url :: Data            -- | The profile page for the user on the Freesound website.
+  username :: Text            -- ^ The user’s username.
+, user_ref  :: Resource User  -- ^ The URI for this resource.
+, user_url :: Data            -- ^ The profile page for the user on the Freesound website.
 } deriving (Eq, Show)
 
 instance FromJSON UserSummary where
@@ -194,15 +194,15 @@ instance FromJSON UserSummary where
   parseJSON _ = mzero
 
 data User = User {
-  user_summary :: UserSummary -- | Summary.
-, user_sounds :: Resource Sounds   -- | The API URI for this user’s sound collection.
-, packs :: Data               -- | The API URI for this user’s pack collection.
-, first_name :: Text          -- | The user’s first name, possibly empty.
-, last_name :: Text           -- | The user’s last name, possibly empty.
-, about :: Text               -- | A small text the user wrote about himself.
-, home_page :: Maybe Data     -- | The user’s homepage, possibly empty.
-, signature :: Text           -- | The user’s signature, possibly empty.
-, dateJoined :: Text          -- | The date the user joined Freesound.
+  user_summary :: UserSummary       -- ^ Summary.
+, user_sounds :: Resource Sounds    -- ^ The API URI for this user’s sound collection.
+, packs :: Data                     -- ^ The API URI for this user’s pack collection.
+, first_name :: Text                -- ^ The user’s first name, possibly empty.
+, last_name :: Text                 -- ^ The user’s last name, possibly empty.
+, about :: Text                     -- ^ A small text the user wrote about himself.
+, home_page :: Maybe Data           -- ^ The user’s homepage, possibly empty.
+, signature :: Text                 -- ^ The user’s signature, possibly empty.
+, dateJoined :: Text                -- ^ The date the user joined Freesound.
 } deriving (Eq, Show)
 
 instance FromJSON User where
@@ -220,13 +220,13 @@ instance FromJSON User where
   parseJSON _ = mzero
 
 data Pack = Pack {
-  pack_ref :: Resource Pack         -- | The URI for this resource.
-, pack_url :: Data                  -- | The URL for this pack’s page on the Freesound website.
-, pack_sounds :: Resource Sounds    -- | The API URI for the pack’s sound collection.
-, pack_user :: Resource UserSummary -- | A JSON object with the user’s username, url, and ref.
-, pack_name :: Text                 -- | The pack’s name.
-, pack_created :: Text              -- | The date when the pack was created.
-, pack_numDownloads :: Integer      -- | The number of times the pack was downloaded. 
+  pack_ref :: Resource Pack         -- ^ The URI for this resource.
+, pack_url :: Data                  -- ^ The URL for this pack’s page on the Freesound website.
+, pack_sounds :: Resource Sounds    -- ^ The API URI for the pack’s sound collection.
+, pack_user :: Resource UserSummary -- ^ A JSON object with the user’s username, url, and ref.
+, pack_name :: Text                 -- ^ The pack’s name.
+, pack_created :: Text              -- ^ The date when the pack was created.
+, pack_numDownloads :: Integer      -- ^ The number of times the pack was downloaded. 
 } deriving (Eq, Show)
 
 instance FromJSON Pack where
