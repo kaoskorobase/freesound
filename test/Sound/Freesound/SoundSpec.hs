8edb3705c694cd4158d8e25d96b997a8f01f0565
Stefan Kersten <stefan@samplecount.com>
2016-02-16 22:52:37 +0100

    Add some more search tests

--- test/Sound/Freesound/SoundSpec.hs
+++ test/Sound/Freesound/SoundSpec.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Sound.Freesound.SoundSpec (spec) where
 
-import Sound.Freesound
-import Sound.Freesound.Test
-import Test.Hspec
+import           Data.Default (def)
+import           Sound.Freesound
+import qualified Sound.Freesound.Sound as Sound
+import           Sound.Freesound.Test
+import           Test.Hspec
 
 spec :: Spec
 spec = do
   describe "search" $ do
-    it "searches" $ fs $ do
-      search_ (include "")
-      return ()
+    it "searches" $ do
+      let p s =    Sound.username s == "k0s"
+                && Sound.name s == "Spiffy Spank"
+      fs (anySatisfy p =<< search_ (include "spank")) `shouldReturn` True
+    it "filters by user name" $ do
+      let p s = Sound.username s == "k0s"
+      fs (allSatisfy p =<< search def def (username "k0s") (include "spank")) `shouldReturn` True
+    it "paginates" $ do
+      l <- fs $ search (Pagination 0 1) def def ""
+      numElems l `shouldBe` 1
